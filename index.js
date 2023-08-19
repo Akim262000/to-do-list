e0399c4f73837f6386969098d146b93173eedb8b
Akim Minko <akim.minko@yandex.ru>
2023-08-19 08:09:10 +0300

    feat: main functionality

---
+++ index.js
@@ -0,0 +1,62 @@
+const todosNode = document.querySelector(".js-todos");
+const inputNode = document.querySelector(".js-input");
+const buttonNode = document.querySelector(".js-button");
+
+let todos = [];
+
+function addTodo(text) {
+  const todo = {
+    text,
+    done: false,
+    id: `${Math.random()}`,
+  };
+
+  todos.push(todo);
+}
+
+function deleteTodo(id) {
+  todos.forEach((todo) => {
+    if (todo.id === id) {
+      todo.done = true;
+    }
+  });
+}
+
+function render() {
+  console.log(todos);
+  let html = "";
+
+  todos.forEach((todo) => {
+    if (todo.done) {
+      return;
+    }
+
+    html += `<div class="list">${todo.text}
+    <button class="todo-delete" data-id='${todo.id}'></button>
+    </div>`;
+  });
+
+  todosNode.innerHTML = html;
+}
+
+buttonNode.addEventListener("click", () => {
+  const text = inputNode.value;
+
+  addTodo(text);
+  render();
+  inputNode.value = "";
+});
+
+todosNode.addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") {
+    return;
+  }
+
+  const id = e.target.dataset.id;
+
+  deleteTodo(id);
+
+  render();
+});
+
+render();
